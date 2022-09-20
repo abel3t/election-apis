b1ce6ca2a7d900f91b6b357d2856d1ebda6476ee
Abel Tran <abeltran.develop@gmail.com>
2022-09-20 13:46:48 +0700

    chore: generate migrations

---
+++ prisma/migrations/20220920064536_initial/migration.sql
@@ -0,0 +1,97 @@
+-- CreateEnum
+CREATE TYPE "EnumAccountStatus" AS ENUM ('Active', 'Pending', 'Deleted');
+
+-- CreateEnum
+CREATE TYPE "EnumAccountRole" AS ENUM ('Admin', 'Church');
+
+-- CreateEnum
+CREATE TYPE "EnumElectionStatus" AS ENUM ('Active', 'Closed');
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "status" "EnumAccountStatus" NOT NULL DEFAULT E'Active',
+    "role" "EnumAccountRole" NOT NULL DEFAULT E'Church',
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Election" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" "EnumElectionStatus" NOT NULL DEFAULT E'Active',
+    "accountId" TEXT NOT NULL,
+
+    CONSTRAINT "Election_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Candidate" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "smallGroup" TEXT,
+    "electionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Candidate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Code" (
+    "id" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "isUsed" BOOLEAN NOT NULL DEFAULT false,
+    "downloaded" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "electionId" TEXT NOT NULL,
+
+    CONSTRAINT "Code_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" TEXT NOT NULL,
+    "codeId" TEXT NOT NULL,
+    "candidateId" TEXT NOT NULL,
+    "electionId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account.email_unique" ON "Account"("email");
+
+-- CreateIndex
+CREATE INDEX "Candidate.electionId_index" ON "Candidate"("electionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vote.electionId_codeId_candidateId_unique" ON "Vote"("electionId", "codeId", "candidateId");
+
+-- AddForeignKey
+ALTER TABLE "Election" ADD CONSTRAINT "Election_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Candidate" ADD CONSTRAINT "Candidate_electionId_fkey" FOREIGN KEY ("electionId") REFERENCES "Election"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Code" ADD CONSTRAINT "Code_electionId_fkey" FOREIGN KEY ("electionId") REFERENCES "Election"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_electionId_fkey" FOREIGN KEY ("electionId") REFERENCES "Election"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_candidateId_fkey" FOREIGN KEY ("candidateId") REFERENCES "Candidate"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_codeId_fkey" FOREIGN KEY ("codeId") REFERENCES "Code"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
